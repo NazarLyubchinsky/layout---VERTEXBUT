9aa6b42499be45ef7d081f6af8a5b7d488d10e46
Nazar <nazar.lyubchinskyi@gmail.com>
2023-03-07 18:56:30 +0200

    Vertexbud

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// BURGER MENU
+
 const BURGER_MENU = document.querySelector('.burger_menu');
 const MOBILE_MENU = document.querySelector('.mobile__menu');
 const HEADER = document.querySelector('header');
@@ -18,6 +20,75 @@ MOBILE_MENU_LINKS.forEach((link) => {
 		switchMobileMenu();
 	})
 })
+
+// 
+// FORM CALLBACK
+// 
+
+const FORM = document.querySelector('.form__callback');
+const NAME = document.getElementById('name');
+const PHONE = document.getElementById('phone');
+const CHECK = document.getElementById('check');
+const SEND_BTN = document.getElementById("send");
+
+// FORM NAME
+NAME.addEventListener('blur', () => {
+	let nameValue = NAME.value;
+
+	let nameRegex = /^[a-zA-Z]+|[Є-ЯҐІЇЁа-їґ]+$/u;
+	let isValidName = nameValue.match(nameRegex);
+
+	let firstCharUpper = nameValue.charAt(0) === nameValue.charAt(0).toUpperCase();
+	let otherCharsLower = nameValue.slice(1).toLowerCase() === nameValue.slice(1);
+
+	if (isValidName && firstCharUpper && otherCharsLower) {
+	} else {
+		alert('Ім’я має містити тільки літери та бути написане з великої першої літери, а всі інші - з маленької');
+		NAME.value = '';
+	}
+});
+// FORM NUMBER
+PHONE.addEventListener('blur', () => {
+	let phoneValue = PHONE.value.trim();
+	let phoneRegex = /^(\+38)?\d{10}$/;
+	let isValidPhone = phoneValue.match(phoneRegex);
+	if (isValidPhone) {
+	} else {
+		alert('Номер телефонускладається з 10 або 12 символів (з префіксом +38)');
+		PHONE.value = '';
+	}
+});
+
+// FORM CHECK
+SEND_BTN.addEventListener("click", function (event) {
+	if (!CHECK.checked) {
+		event.preventDefault();
+		alert("Будь ласка, погодьтесь з політикою конфіденційності");
+	}
+});
+
+// LOCAL_STORAGE
+if (localStorage.getItem('name')) {
+	NAME.value = localStorage.getItem('name');
+}
+
+if (localStorage.getItem('phone')) {
+	PHONE.value = localStorage.getItem('phone');
+}
+
+if (localStorage.getItem('check')) {
+	CHECK.checked = localStorage.getItem('check');
+}
+
+FORM.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	localStorage.setItem('name', NAME.value);
+	localStorage.setItem('phone', PHONE.value);
+	localStorage.setItem('check', CHECK.checked);
+
+	FORM.submit();
+});
 
 
 
